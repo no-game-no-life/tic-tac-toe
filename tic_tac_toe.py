154555893627ff1027a56a9e6177a87cb85910be
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2018-02-07 18:20:46 -0700

    layout basic gameplay functions

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,17 @@
 # play tic tac toe against an ai
+
+# create, print, label game board
+def make_board():
+    pass
+
+# user is player one, ai is player two
+def turns():
+    pass
+
+# ai analyzes board for best move
+def ai_calc():
+    pass
+
+# main method
+def main():
+    pass
